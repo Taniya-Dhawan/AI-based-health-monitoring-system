631410ed6acb270adf3ea262152e366c8d9bb923
Taniya Dhawan <taniyadhawan724@gmail.com>
2026-04-23 17:36:05 +0530

    Add files via upload
    AI based real-time health monitoring system

---
+++ health_project/app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, jsonify
+import random, time
+
+app = Flask(__name__)
+
+def generate_data():
+    return {
+        "patient_id": random.randint(1000, 1010),
+        "heart_rate": random.randint(60, 140),
+        "temperature": round(random.uniform(36, 40), 2),
+        "oxygen": random.randint(85, 100),
+        "timestamp": time.strftime("%H:%M:%S")
+    }
+
+@app.route("/")
+def login():
+    return render_template("login.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("index.html")
+
+@app.route("/data")
+def data():
+    d = generate_data()
+
+    if d["heart_rate"] > 120 or d["temperature"] > 38 or d["oxygen"] < 90:
+        d["alert"] = "CRITICAL"
+    else:
+        d["alert"] = "NORMAL"
+
+    if d["heart_rate"] > 110:
+        d["prediction"] = "HIGH RISK"
+    else:
+        d["prediction"] = "LOW RISK"
+
+    return jsonify(d)
+
+if __name__ == "__main__":
+    app.run(debug=True)
